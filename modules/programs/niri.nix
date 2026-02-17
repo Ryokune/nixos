630d1312b188fedfbeba5769918d85b513dec0bf
fish <kiokunee@gmail.com>
2026-02-18 01:45:36 +0800

    feat: add udisk and gvfs service

--- modules/programs/niri.nix
+++ modules/programs/niri.nix
@@ -7,5 +7,9 @@
       xdg.portal.config.niri = {
         "org.freedesktop.impl.portal.FileChooser" = [ "gtk" ]; # or "kde"
       };
+
+      # Would probably be better to be placed in a generic WM/DE Module in the future.
+      services.gvfs.enable = true;
+      services.udisks2.enable = true;
     };
 }
