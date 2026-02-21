be718521506174d3ff3caae15ecb5000efefcb2b
fish <kiokunee@gmail.com>
2026-02-21 14:08:37 +0800

    feat(programs/gamescope): add gamescope

---
+++ modules/programs/gamescope.nix
@@ -0,0 +1,15 @@
+{ self, ... }:
+{
+  flake.nixosModules.gamescope =
+    { pkgs, ... }:
+    {
+      programs.gamescope = {
+        enable = true;
+        capSysNice = true; # Recommended for performance
+      };
+      environment.systemPackages = with pkgs; [
+        gamescope
+        gamescope-wsi # Required for HDR support
+      ];
+    };
+}
