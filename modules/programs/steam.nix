e90b0b5b7321ba89e78bf0a3d1d43dcaad389ed1
fish <kiokunee@gmail.com>
2026-02-21 14:08:04 +0800

    fix(programs/steam): add `-system-composer` to fix black screen issue

--- modules/programs/steam.nix
+++ modules/programs/steam.nix
@@ -1,12 +1,16 @@
 { self, ... }:
 {
   flake.nixosModules.steam =
-    { ... }:
+    { pkgs, ... }:
     {
       programs.steam = {
         enable = true;
         remotePlay.openFirewall = true;
         dedicatedServer.openFirewall = true;
+      };
+      # Fixes black screen.
+      programs.steam.package = pkgs.steam.override {
+        extraArgs = "-system-composer";
       };
     };
 }
