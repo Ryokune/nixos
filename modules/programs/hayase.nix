0d63891e4edd69a17c33797409e9ea44b86f96f8
fish <kiokunee@gmail.com>
2026-02-21 17:45:12 +0800

    feat(hayase): use seccomp `!xyz` instead of manually dropping

--- modules/programs/hayase.nix
+++ modules/programs/hayase.nix
@@ -24,8 +24,7 @@
         private-dev
         private-tmp
         private-etc hosts,resolv.conf,ssl,ca-certificates
-        #seccomp
-        seccomp.drop ptrace,process_vm_readv,process_vm_writev,keyctl,add_key,request_key
+        seccomp !mount,!umount2,!pivot_root,!chroot,!membarrier
 
         noinput
         nogroups
