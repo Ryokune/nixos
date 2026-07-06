531d83736fbfb6364f43c9e54c6f2b2b67d9715d
fish <kiokunee@gmail.com>
2026-07-07 06:31:55 +0800

    hosts/siesta(virtualcam): add virtual cam

--- modules/hosts/siesta/_modules/virtualcam.nix
+++ modules/hosts/siesta/_modules/virtualcam.nix
@@ -1,11 +1,10 @@
 { config, ... }:
 {
-  # Load the v4l2loopback kernel module
   boot.extraModulePackages = with config.boot.kernelPackages; [ v4l2loopback ];
   boot.kernelModules = [ "v4l2loopback" ];
 
-  # Configure the virtual device
   boot.extraModprobeConfig = ''
-    options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+    options v4l2loopback devices=2 video_nr=1,2 card_label="OBS Cam, Camera" exclusive_caps=1
   '';
+  #programs.obs-studio.enableVirtualCamera = true;
 }
